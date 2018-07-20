76432aa9574ac13cf0851523211a7776f397e0d7
zc <yyzc2513@163.com>
2018-07-20 15:50:42 +0800

    .

--- src/components/element/table.vue
+++ src/components/element/table.vue
@@ -1,7 +1,12 @@
 <template>
   <div style="margin-top:10px;">
-      <router-link v-for="(item,index) in btncon" :key='index' v-text="item.con" :to='item.uri' ></router-link>
-      
+      <section style="margin-bottom:20px;">
+        <el-button type='info' @click="table">table表格</el-button>
+        <el-button type='info' @click="nav">导航栏</el-button>
+      </section>
+      <div>
+        <router-link v-for="(item,index) in btncon" :key='index' v-text="item.con" :to='item.uri' ></router-link>
+      </div>
       <div style="margin-top:20px;">
             <router-view/>          
       </div>
@@ -11,15 +16,23 @@
   export default {
     data() {
       return {
-        btncon:[{uri:'/elementUi/tablestyle',con:'表格样式添加及方法'}
+        btncon:'',
+      }
+    },
+    methods:{
+      table(){
+        this.btncon =[{uri:'/elementUi/tablestyle',con:'表格样式添加及方法'}
                 ,{uri:'/elementUi/tableEven',con:'表格方法及事件'}
                 ,{uri:'/elementUi/tableradio',con:'完整单选全选'}
                 ,{uri:'/elementUi/tableselect',con:'单层JSON循环'}
                 ,{uri:'/elementUi/tableJson',con:'json带有操作的选择'}
-                ,{uri:'/elementUi/moreTable',con:'插件封装-多功能表格'},
-                {uri:'/nav',con:'导航栏'}],
+                ,{uri:'/elementUi/moreTable',con:'插件封装-多功能表格'}
+                ]
+      },
+      nav(){
+        this.btncon = [{uri:'/nav',con:'导航栏'}]
       }
-    }
+    },
   }
 </script>
 <style lang="scss" scoped>
